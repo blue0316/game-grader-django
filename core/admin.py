e65a13a78d6b21cf6b9c05f130be53215be9ed17
Sudhanshu Devmorari <sd.empiricinfotech2@gmail.com>
2023-01-12 18:11:03 +0530

    Created the Team-Detail & Invite-Team model & Created functionality for Add-Team-Detail.

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from core.models import User
+from core.models import User, InviteTeam, TeamDetail
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username','email','password','profile_pic')
+    list_display = ('uuid','username','email','password','role')
+
+@admin.register(TeamDetail)
+class InviteTeamAdmin(admin.ModelAdmin):
+    list_display = ('user','team_name','team_code')
+
+@admin.register(InviteTeam)
+class TeamDetailAdmin(admin.ModelAdmin):
+    list_display = ('invite_by','invite_to','team')
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, FormView
 from django.views import View
-from core.models import User
+from core.models import User, TeamDetail, InviteTeam
 from core.forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
@@ -21,14 +21,31 @@ class SignupView(View):
         email = request.POST['email']
         password = request.POST['password']
         confirm_pass = request.POST['confirm_password']
+        code = request.POST['team_code']
+
         if username !='':
+            if User.objects.filter(username=username).exists():
+                messages.error(request, "Username already exists.Please you can choose another one. ")
+                return redirect('signup')
             if email !='':
                 if password !='':
                     if confirm_pass !='':
                         if password == confirm_pass:
-                            user = User.objects.create(username=username, email=email, password=password)
-                            user.save()
-                            return redirect('login')
+                            uu_id = str(uuid.uuid4().hex[:8])
+                            if code=='':
+                                user = User.objects.create(username=username, email=email, password=password, uuid=uu_id)
+                                user.save()
+                                return redirect('login')
+                            else:
+                                if TeamDetail.objects.filter(team_code=code).exists():
+                                    user = User.objects.create(username=username, email=email, password=password, uuid=uu_id)
+                                    user.save()
+                                    team_detail = TeamDetail.objects.get(team_code=code)
+                                    invite_team = InviteTeam.objects.create(invite_by=team_detail.user, invite_to=user, team=team_detail)
+                                    invite_team.save()
+                                    return redirect('dashboard')
+                                else:
+                                    messages.error(request, 'Please Enter Valid Team-Code...')
                         else:
                             messages.error(request, "Password and Confirm Password are not same.")
                     else:
@@ -76,7 +93,7 @@ class LoginView(View):
         else:
             username = request.POST['user_name']
             password = request.POST['pass_word']
-            if User.objects.filter(username=username, password=password).exists():   
+            if User.objects.filter(username=username, password=password).exists():
                 user = User.objects.get(username=username, password=password)
                 print("user: ",user)
                 login(request, user)
@@ -93,11 +110,46 @@ def send_invite_mail(user_mail, user):
     body = f"For joining the team Click the link: {link}"
     send_mail(subject=subject, from_email = gmail_user , message=body ,recipient_list =to, fail_silently=False)
 
+
+class DashboardView(View):
+    def get(self,request):
+        # if request.user.is_authenticated:
+            return render(request, 'dashboard.html')
+
+
+class ManageTeamView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'manageteam.html')
+
+class AddTeamDetailView(View):
+    def get(self, request, *args, **kwargs):
+        team_code = str(uuid.uuid4().hex[:8])
+        return render(request, 'addteam.html', {'team_code':team_code})
+    
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        team_name = request.POST['team_name']
+        team_code = request.POST['team_code']
+        # team_code = str(uuid.uuid4().hex[:8])
+
+        if team_name != '':
+            if team_code != '':
+                team = TeamDetail.objects.create(user=user,team_name=team_name, team_code=team_code)
+                team.save()
+                return redirect('manageteam.html')
+            else:
+                messages.error(request, "Please Enter Team-Code...")
+        else:
+            messages.error(request, "Please Enter Team-Name...")
+        return render(request, 'addteam.html')
+        
+        
 class InviteTeamView(View):
     def get(self, request, *args, **kwargs):
         # if request.user.is_authenticated:
         #     user = request.user
         #     if user=="Admin":
+                print("--->>",request.user)
                 return render(request, 'invite.html')
             # else:
             #     return render(request, 'dashboard.html',{'error':'Error: You are not Admin...'})
@@ -107,20 +159,31 @@ class InviteTeamView(View):
         l_name = request.POST['Lname']
         email = request.POST['Email']
         role = request.POST['bordered-radio']
+        code = request.POST['team_code']
 
         if f_name != '':
             if l_name != '':
                 if email != '':
                     if role != '':
-                        uu_id = str(uuid.uuid4().hex[:8])
-                        username = f_name+'  '+l_name
-                        user = User.objects.create(first_name=f_name, last_name=l_name, email=email, role=role, username=username, uuid=uu_id)
-                        user.save()
-                        if user is not None:
-                            send_invite_mail(email, user)
-                            return redirect('dashboard')
+                        if code != '':
+                            uu_id = str(uuid.uuid4().hex[:8])
+                            username = f_name+'-'+l_name
+                            if TeamDetail.objects.filter(team_code=code).exists():
+                                user = User.objects.create(first_name=f_name, last_name=l_name, email=email, role=role, username=username, uuid=uu_id)
+                                user.save()
+                                if user is not None:
+                                    send_invite_mail(email, user)
+                                    team_detail = TeamDetail.objects.get(team_code=code)
+                                    invite_team = InviteTeam.objects.create(invite_by=request.user, invite_to=user, team=team_detail)
+
+                                    invite_team.save()
+                                    return redirect('dashboard')
+                                else:
+                                    messages.error(request, 'Invite not sent...')
+                            else:
+                                messages.error(request, 'Please Enter Valid Team-Code...')
                         else:
-                            messages.error(request, 'Invite not sent...')
+                            messages.error(request, 'Please Enter Team-Code...')
                     else:
                         messages.error(request, "Please Select Role...")
                 else:
@@ -130,7 +193,3 @@ class InviteTeamView(View):
         else:
             messages.error(request, "Please Enter First-name...")
         return redirect('invite_team')
-        
-class DashboardView(View):
-    def get(self,request):
-        return render(request, 'dashboard.html')
