65c22e15ff1b88b8355702c9c1741af82f6e5767
md-empiric <md.empiric@gmail.com>
2023-01-18 10:24:20 +0530

    Completed "Name & description" section in add team member page

--- static/js/addteammember.js
+++ static/js/addteammember.js
@@ -46,3 +46,22 @@ function dropdown() {
     },
   };
 }
+// The DOM element you wish to replace with Tagify
+var input = document.querySelector('input[name=basic]');
+
+// initialize Tagify on the above input node reference
+new Tagify(input)
+tinymce.init({
+  selector: 'textarea',
+  plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+  toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+  tinycomments_mode: 'embedded',
+  tinycomments_author: 'Author name',
+  mergetags_list: [
+    { value: 'First.Name', title: 'First Name' },
+    { value: 'Email', title: 'Email' },
+  ],
+  // content_css : '../static/css/texteditor.css'
+  content_style: 'div { margin: 10px; border: 5px solid red; padding: 3px; } ' +
+    '.blue { color: blue; } .red { color: red; }'
+});
